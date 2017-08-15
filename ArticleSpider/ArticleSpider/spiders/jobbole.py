afd39fe0f9b64e3114163a2ec9d0ce87c527a435
niro <nirolee@foxmail.com>
2017-08-15 17:55:01 +0800

    写了点匹css配文章的参数

--- ArticleSpider/ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import re
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobole.com']
-    start_urls = ['http://blog.jobole.com/']
+    start_urls = ['http://blog.jobole.com/112127']
 
     def parse(self, response):
+        title = response.css(".entry-header h1::text").extract()[0]
+        time = response.css(".entry-meta-hide-on-mobile::text").extract()[0].strip().replace("·","")
+        praise_nums = response.css(".vote-post-up h10::text").extract()[0]
+        fav_nums = response.css(".bookmark-btn::text").extract()[0]
+        match_nums = re.match(".*(\d+).*",fav_nums)
+        if(match_nums):
+            match_nums.group(1)
+
         pass
