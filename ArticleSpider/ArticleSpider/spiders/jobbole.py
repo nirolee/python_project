366f595923693535658be3da84673bf71a65f11e
niro <nirolee@foxmail.com>
2017-08-17 00:25:15 +0800

    20170817

--- ArticleSpider/ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
-
+from scrapy.http import request
+from urllib import parse
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/112170']
+    start_urls = ['http://blog.jobbole.com/category/php-programmer/']
 
     def parse(self, response):
+        post_urls = response.css("#archive .floated-thumb .post-thumb a::attr(href)").extract()
+        for post_url in post_urls:
+            yield request(url=parse.urljoin(response.url, post_url), callback=self.parse_detail())
+        next_url = response.css(".next.page-numbers::attr(href)").extract_first()
+        if next_url:
+            yield request(url=parse.urljoin(response.url, next_url), callback=self.parse())
+        print(post_urls)
+
+    def parse_detail(self, response):
         # title = response.xpath("//div[@class='entry-header']/h1/text()").extract()[0]
         # time = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace("·","")
         # praise_nums = response.xpath("//span[contains(@class,'vote-post-up')]/h10/text()").extract()[0]
@@ -18,7 +28,7 @@ class JobboleSpider(scrapy.Spider):
         fav_nums = response.css(".bookmark-btn::text").extract()[0]
         match_nums = re.match(".*(\d+).*", fav_nums)
         if(match_nums):
-            match_nums.group(1)
+             fav_nums = match_nums.group(1)
         content = response.css(".category-it-tech p::text").extract()
         for i, p in enumerate(content):
             print(i, p)
