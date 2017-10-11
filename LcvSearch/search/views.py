dfca416118cb114e7ff8dd6c7d5633d8fc7747f5
niro <nirolee@foxmail.com>
2017-10-11 17:21:39 +0800

    LCV

--- LcvSearch/search/views.py
+++ LcvSearch/search/views.py
@@ -4,18 +4,18 @@ from django.views.generic.base import View
 from search.models import ArticleType
 
 
-class SearchSuggest(View):
-    def get(self, request):
-        key_words = request.GET.get('s', '')
-
-        if key_words:
-            s = ArticleType.search()
-            s = s.suggest('my_suggest', key_words, completion={
-                "field": "suggest", "fuzzy": {
-                    "fuzzness": 2
-                },
-                "size": 10
-            })
-            suggestions = s.execute_suggest()
-            for match in suggestions:
-                pass
+# class SearchSuggest(View):
+#     def get(self, request):
+#         key_words = request.GET.get('s', '')
+#
+#         if key_words:
+s = ArticleType.search()
+s = s.suggest('my_suggest', 'linux', completion={
+    "field": "suggest", "fuzzy": {
+        "fuzziness": 2
+    },
+    "size": 10
+})
+suggestions = s.execute_suggest()
+for match in suggestions:
+    pass
