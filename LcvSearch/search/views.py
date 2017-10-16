bf4e5a4e057da44cd0da6d3b4387f2509a90ca53
niro <nirolee@foxmail.com>
2017-10-16 18:01:36 +0800

    lcv

--- LcvSearch/search/views.py
+++ LcvSearch/search/views.py
@@ -10,12 +10,13 @@ from search.models import ArticleType
 #
 #         if key_words:
 s = ArticleType.search()
-s = s.suggest('my_suggest', 'linux', completion={
-    "field": "suggest", "fuzzy": {
-        "fuzziness": 2
+s = s.suggest('my_suggest', 'python', completion={
+    "field": "suggest",
+    "fuzzy": {
+        "fuzziness": 1
     },
     "size": 10
 })
 suggestions = s.execute_suggest()
-for match in suggestions:
+for match in suggestions['my_suggest'][0].options:
     pass
