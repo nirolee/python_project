0abe5ae03dd209f4cc0222b0b85dfbbf914b474d
niro <nirolee@foxmail.com>
2017-10-18 17:40:22 +0800

    Lcv

--- LcvSearch/search/views.py
+++ LcvSearch/search/views.py
@@ -3,7 +3,9 @@ from django.views.generic.base import View
 # Create your views here.
 from search.models import ArticleType
 from django.http import HttpResponse
+from elasticsearch import Elasticsearch
 import json
+client = Elasticsearch(hosts='127.0.0.1')
 
 class SearchSuggest(View):
     def get(self, request):
@@ -27,3 +29,43 @@ class SearchSuggest(View):
 
 
 class SearchView(View):
+    def get(self, request):
+        key_words = request.GET.get('q', '')
+        response = client.search(
+            index= "jobbole",
+            body= {
+                "query": {
+                    "multi_match": {
+                        "query": key_words,
+                        "fields": ["title", "tags", "content"]
+                    }
+                },
+                "from": 0,
+                "size": 10,
+                "highlight": {
+                    "pre_tags": ["<span class='keyword'>"],
+                    "post_tags": ["</span>"],
+                    "fields": {
+                        "title": {},
+                        "content": {}
+                    }
+                }
+            }
+        )
+        hits_list = []
+        total_nums = response["hits"]["total"]
+        for hit in response["hits"]["hits"]:
+            hit_dict = {}
+            if "title" in hit["highlight"]:
+                hit_dict["title"] = hit["highlight"]["title"][0]
+            if "content" in hit["highlight"]:
+                hit_dict["content"] = hit["highlight"]["content"][0]
+            else:
+                hit_dict["content"] = hit["_source"]["content"][0]
+            hit_dict["create_time"] = hit["_source"]["create_time"]
+            hit_dict["url"] = hit["_source"]["url"]
+            hit_dict["score"] = hit["_score"]
+            hits_list.append(hit_dict)
+
+        return render(request, "result.html", {"all_hits": hits_list, "key_words": key_words, "total_nums": total_nums,
+           "page_nums": total_nums/10})
