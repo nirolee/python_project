0717396bdd77ad7139f53841d6f163774b998640
niro <nirolee@foxmail.com>
2018-02-08 17:40:10 +0800

    tensorflow

---
+++ python_learn/1.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+from sys import argv
+
+scrapt, username = argv
+
+print("this is the %s the  scrapt %s" % (username, scrapt))
+print("this is the username %s" % username)
+
+
